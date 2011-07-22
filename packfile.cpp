ea5cc6f1cc768e7bc38f9822180ea4c1f99a4112
lxc <lxcypp@163.com>
2011-07-22 19:50:52 +0800

    1. modifing...

--- packfile.cpp
+++ packfile.cpp
@@ -1,13 +1,58 @@
 #include "packfile.h"
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+LPackFile::LPackFile()
+{
+	m_fd = 0;
+	m_mmap = 0;
+	m_filesize = 0;
+}
 
 LPackFile::LPackFile(const char *filename)
 {
+	m_fd = 0;
+	m_mmap = 0;
+	m_filesize = 0;
+	OpenFile(filename);
 }
 
-void LPackFile::AppendFile(const char*srcfilename, const char* destfilename)
+LPackFile::~LPackFile()
+{
+	if(m_fd>0)
+	{
+		close(m_fd);
+	}
+	if(m_mmap!=0)
+	{
+		munmap(m_mmap, m_filesize);
+	}
+	m_fd = 0;
+	m_mmap = 0;
+}
+
+void LPackFile::OpenFile(const char *filename)
+{
+	if(access(filename,0)!=0)
+	{
+		m_fd = open(filename, O_CREAT);
+	}else
+	{
+		m_fd = open(filename, O_RDWR);
+	}
+	m_mmap = mmap(NULL, m_filesize, PROT_READ, MAP_SHARED, m_fd, 0);
+}
+
+void LPackFile::AppendSubFile(const char*srcfilename, const char* destfilename)
 {
 }
-void LPackFile::DeleteFile(const char*destfilename)
+void LPackFile::DeleteSubFile(const char*destfilename)
+{
+}
+void LPackFile::ReadSubFile(const char*destfilename)
 {
 }
 void LPackFile::PackFile()
--- packfile.h
+++ packfile.h
@@ -1,17 +1,37 @@
 #ifndef _FILEPACK_H_
 #define _FILEPACK_H_
+#include <stdint.h>
+#include <map>
 /*
 文件格式：
 索引区+内容区
-索引区=256KB的块（可以放256*2=512个文件索引）
+索引区=256KB的块（可以放256*1024/512=512个文件索引）
 索引区=索引+索引+索引+索引...
 索引=512B的块=文件目录名（508B）+偏移（4B）
+索引区的最后一个索引是指向下一个索引区的偏移值
+索引的第一个4Byte=0，表示该条记录为空
 */
+typedef struct _FILEINDEX_
+{
+	uint32_t m_size;		//文件大小(小于4G)，如果==0，且m_filename==“”则表示这个记录为空
+	char	m_filename[500];	//文件名（含路径）
+	uint64_t m_offset;		//文件偏移
+}FILEINDEX,*PFILEINDEX;
+typedef FILEINDEX FILEBLOCK[512];
+
 class LPackFile{
+	std::map<const char *, uint64_t> m_FileOffset;
+	int m_fd;
+	void *m_mmap;
+	uint64_t	m_filesize;
 public:
+	LPackFile();
 	LPackFile(const char *filename);
-	void AppendFile(const char*srcfilename, const char* destfilename);
-	void DeleteFile(const char*destfilename);
+	~LPackFile();
+	void OpenFile(const char * filename);
+	void AppendSubFile(const char*srcfilename, const char* destfilename);
+	void DeleteSubFile(const char*destfilename);
+	void ReadSubFile(const char*destfilename);
 	void PackFile();
 };
 
